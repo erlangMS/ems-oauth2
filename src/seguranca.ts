bacff3095dce61ab5d012e5c43c6b9017ad2ab44
Renato Carauta Ribeiro <rcarauta6@gmail.br>
2017-09-20 11:44:05 -0300

    retirada do module user

--- src/seguranca.ts
+++ src/seguranca.ts
@@ -2,7 +2,6 @@
 
 export {AuthGuard} from "./app/_guards/auth.guard";
 export {DefaultHeaders} from "./app/_headers/default.headers";
-export {User} from './app/_models/user';
 export {AuthenticationService} from "./app/_services/authentication.service";
 export { RedirectService } from './app/_redirect/redirect.service';
 export { DefaultResponse } from './app/_response/default.response.service';
